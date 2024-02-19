b94438d38d8031cd3d3c9f70b39b149de248a15c
Kristofer Stensland <kristoferstensland@Kristofers-MacBook-Pro.local>
2024-02-18 20:38:06 -0800

    reprint example working

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
+from reprint import output
+import time
 
 class Matrix:
     def __init__(self, width_in, height_in):
         self.width = width_in
         self.height = height_in
-        self.rows = self._create_empty_matrix() 
+        self.rows = self._create_empty_matrix()
 
     def _create_empty_matrix(self):
         _matrix = []
@@ -15,24 +17,37 @@ class Matrix:
                 # row.append(False)
             _matrix.append(row)
         return _matrix
+    
+    def next_step(self):
+        for x in range(len(self.rows)):
+            row = self.rows[x]
+            for y in range(len(row)):
+                # iterate on this decision
+                self.rows[x][y] = not self.rows[x][y]
 
 class MatrixRenderer(ABC):
     # Eventually we will have one for CLI output, and one for Raspberry Pi output
     @abstractmethod
-    def generate_output(self, matrix):
+    def generate_output(self, matrix, output_obj):
         pass
 
 class CLIMatrixRenderer(MatrixRenderer):
-    def generate_output(self, matrix):
+    def generate_output(self, matrix, output_obj, i):
         header = "--" + ("---" * (matrix.width))
-        print(header)
+        for pos in range(len(output_obj)-1, 0, -1):
+            output_obj.remove(output_obj[pos])
         for row in matrix.rows:
-            row_str = "|"
-            for item in row:
-                row_str += (("   " if item else " X "))
-            row_str += "|"
-            print(row_str)
-        print(header)
+            
+            output_obj.append((f"HELLO  -  {i}"))
+
+        # print(header)
+        # for row in matrix.rows:
+        #     row_str = "|"
+        #     for item in row:
+        #         row_str += (("   " if item else " X "))
+        #     row_str += "|"
+        #     print(row_str)
+        # print(header)
 
 class GOL:
     def __init__(self, dimension_in=20, total_steps_in=10):
@@ -40,7 +55,13 @@ class GOL:
         self.total_steps = total_steps_in
 
         renderer = CLIMatrixRenderer()  # eventually will change for rasp pi
-        renderer.generate_output(self.matrix)
+
+        with output(output_type="list", initial_len=dimension_in) as output_list:
+            for i in range(self.total_steps):
+                time.sleep(1)
+                # self.matrix.next_step()
+                renderer.generate_output(self.matrix, output_list, i)
+                # print(i)
 
 def main():
     print("starting game")
